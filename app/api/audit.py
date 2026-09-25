MbedLabs/bud

feat(audit): record security-relevant events and show them to administrators

app/api/audit.py
@@ -1,0 +1,77 @@
+"""Audit log API (admin only): read the audit events, newest first, with filters.
+
+There is no endpoint that changes or deletes an event.
+"""
+
+from datetime import datetime
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api.auth import require_role
+from app.db.database import get_db
+from app.models.audit import AuditEvent
+from app.models.user import User, UserRole
+from app.schemas.audit import AuditEventPage, AuditEventResponse
+
+router = APIRouter()
+
+require_admin = require_role(UserRole.admin)
+
+
+@router.get("", response_model=AuditEventPage)
+async def list_audit_events(
+    actor_user_id: Optional[int] = None,
+    action: Optional[str] = None,
+    target_type: Optional[str] = None,
+    target_id: Optional[str] = None,
+    product_id: Optional[int] = None,
+    outcome: Optional[str] = None,
+    since: Optional[datetime] = None,
+    until: Optional[datetime] = None,
+    limit: int = Query(50, ge=1, le=500),
+    offset: int = Query(0, ge=0),
+    db: AsyncSession = Depends(get_db, scope="function"),
+    _admin: User = Depends(require_admin),
+):
+    """List audit events matching every given filter; an action filter ending in "." is a prefix."""
+    conditions = []
+    if actor_user_id is not None:
+        conditions.append(AuditEvent.actor_user_id == actor_user_id)
+    if action:
+        if action.endswith("."):
+            conditions.append(AuditEvent.action.startswith(action))
+        else:
+            conditions.append(AuditEvent.action == action)
+    if target_type:
+        conditions.append(AuditEvent.target_type == target_type)
+    if target_id:
+        conditions.append(AuditEvent.target_id == target_id)
+    if product_id is not None:
+        conditions.append(AuditEvent.product_id == product_id)
+    if outcome:
+        conditions.append(AuditEvent.outcome == outcome)
+    if since is not None:
+        conditions.append(AuditEvent.occurred_at >= since)
+    if until is not None:
+        conditions.append(AuditEvent.occurred_at < until)
+
+    total = await db.scalar(select(func.count(AuditEvent.id)).where(*conditions))
+    rows = (
+        await db.execute(
+            select(AuditEvent, User.full_name)
+            .outerjoin(User, User.id == AuditEvent.actor_user_id)
+            .where(*conditions)
+            .order_by(AuditEvent.occurred_at.desc(), AuditEvent.id.desc())
+            .limit(limit)
+            .offset(offset)
+        )
+    ).all()
+    items = []
+    for event, actor_name in rows:
+        item = AuditEventResponse.model_validate(event)
+        item.actor_name = actor_name
+        items.append(item)
+    return AuditEventPage(items=items, total=total or 0)

app/models/audit.py
@@ -1,0 +1,36 @@
+"""Audit log: one row per security-relevant event, written with the action it records."""
+
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import JSON, DateTime, Index, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+
+class AuditEvent(Base):
+    """Who did what to accounts, access, configuration or data, and with what outcome."""
+
+    __tablename__ = "audit_events"
+    __table_args__ = (
+        Index("ix_audit_events_occurred_at", "occurred_at"),
+        Index("ix_audit_events_actor_user_id", "actor_user_id"),
+        Index("ix_audit_events_action", "action"),
+        Index("ix_audit_events_target", "target_type", "target_id"),
+        Index("ix_audit_events_product_id", "product_id"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    occurred_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
+    actor_user_id: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    actor_type: Mapped[str] = mapped_column(String(20), nullable=False)
+    action: Mapped[str] = mapped_column(String(80), nullable=False)
+    target_type: Mapped[Optional[str]] = mapped_column(String(50), nullable=True)
+    target_id: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    product_id: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    request_id: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    ip: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    user_agent: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    outcome: Mapped[str] = mapped_column(String(20), nullable=False, default="success")
+    details: Mapped[Optional[dict]] = mapped_column(JSON, nullable=True)

app/schemas/audit.py
@@ -1,0 +1,34 @@
+"""Audit log responses."""
+
+from datetime import datetime
+from typing import Any, List, Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+class AuditEventResponse(BaseModel):
+    """One audit event, with the acting user's name when the user still exists."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    occurred_at: datetime
+    actor_user_id: Optional[int] = None
+    actor_name: Optional[str] = None
+    actor_type: str
+    action: str
+    target_type: Optional[str] = None
+    target_id: Optional[str] = None
+    product_id: Optional[int] = None
+    request_id: Optional[str] = None
+    ip: Optional[str] = None
+    user_agent: Optional[str] = None
+    outcome: str
+    details: Optional[dict[str, Any]] = None
+
+
+class AuditEventPage(BaseModel):
+    """A page of audit events, newest first, and the total that match the filter."""
+
+    items: List[AuditEventResponse]
+    total: int

ui/src/api/client.ts
@@ -259,6 +259,46 @@ export const groupsApi = {
     (await api.delete<Group>(`/groups/${id}/grants/${grantId}`)).data,
 }
 
+export interface AuditEvent {
+  id: number
+  occurred_at: string
+  actor_user_id: number | null
+  actor_name: string | null
+  actor_type: string
+  action: string
+  target_type: string | null
+  target_id: string | null
+  product_id: number | null
+  request_id: string | null
+  ip: string | null
+  user_agent: string | null
+  outcome: string
+  details: Record<string, unknown> | null
+}
+
+export interface AuditEventPage {
+  items: AuditEvent[]
+  total: number
+}
+
+export interface AuditFilters {
+  actor_user_id?: number
+  action?: string
+  target_type?: string
+  target_id?: string
+  product_id?: number
+  outcome?: string
+  since?: string
+  until?: string
+  limit?: number
+  offset?: number
+}
+
+export const auditApi = {
+  list: async (filters: AuditFilters = {}) =>
+    (await api.get<AuditEventPage>('/audit', { params: filters })).data,
+}
+
 export const usersApi = {
   list: async (): Promise<User[]> => {
     const response = await api.get<User[]>('/users')

MbedLabs/bloom

app/api/audit.py
@@ -1,0 +1,77 @@
+"""Audit log API (admin only): read the audit events, newest first, with filters.
+
+There is no endpoint that changes or deletes an event.
+"""
+
+from datetime import datetime
+from typing import Optional
+
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database import get_db
+from app.core.security import require_role
+from app.models.audit import AuditEvent
+from app.models.user import User, UserRole
+from app.schemas.audit import AuditEventPage, AuditEventResponse
+
+router = APIRouter()
+
+require_admin = require_role(UserRole.admin)
+
+
+@router.get("", response_model=AuditEventPage)
+async def list_audit_events(
+    actor_user_id: Optional[int] = None,
+    action: Optional[str] = None,
+    target_type: Optional[str] = None,
+    target_id: Optional[str] = None,
+    project_id: Optional[int] = None,
+    outcome: Optional[str] = None,
+    since: Optional[datetime] = None,
+    until: Optional[datetime] = None,
+    limit: int = Query(50, ge=1, le=500),
+    offset: int = Query(0, ge=0),
+    db: AsyncSession = Depends(get_db, scope="function"),
+    _admin: User = Depends(require_admin),
+):
+    """List audit events matching every given filter; an action filter ending in "." is a prefix."""
+    conditions = []
+    if actor_user_id is not None:
+        conditions.append(AuditEvent.actor_user_id == actor_user_id)
+    if action:
+        if action.endswith("."):
+            conditions.append(AuditEvent.action.startswith(action))
+        else:
+            conditions.append(AuditEvent.action == action)
+    if target_type:
+        conditions.append(AuditEvent.target_type == target_type)
+    if target_id:
+        conditions.append(AuditEvent.target_id == target_id)
+    if project_id is not None:
+        conditions.append(AuditEvent.project_id == project_id)
+    if outcome:
+        conditions.append(AuditEvent.outcome == outcome)
+    if since is not None:
+        conditions.append(AuditEvent.occurred_at >= since)
+    if until is not None:
+        conditions.append(AuditEvent.occurred_at < until)
+
+    total = await db.scalar(select(func.count(AuditEvent.id)).where(*conditions))
+    rows = (
+        await db.execute(
+            select(AuditEvent, User.full_name)
+            .outerjoin(User, User.id == AuditEvent.actor_user_id)
+            .where(*conditions)
+            .order_by(AuditEvent.occurred_at.desc(), AuditEvent.id.desc())
+            .limit(limit)
+            .offset(offset)
+        )
+    ).all()
+    items = []
+    for event, actor_name in rows:
+        item = AuditEventResponse.model_validate(event)
+        item.actor_name = actor_name
+        items.append(item)
+    return AuditEventPage(items=items, total=total or 0)

app/models/audit.py
@@ -1,0 +1,36 @@
+"""Audit log: one row per security-relevant event, written with the action it records."""
+
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import JSON, DateTime, Index, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class AuditEvent(Base):
+    """Who did what to accounts, access, configuration or data, and with what outcome."""
+
+    __tablename__ = "audit_events"
+    __table_args__ = (
+        Index("ix_audit_events_occurred_at", "occurred_at"),
+        Index("ix_audit_events_actor_user_id", "actor_user_id"),
+        Index("ix_audit_events_action", "action"),
+        Index("ix_audit_events_target", "target_type", "target_id"),
+        Index("ix_audit_events_project_id", "project_id"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    occurred_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
+    actor_user_id: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    actor_type: Mapped[str] = mapped_column(String(20), nullable=False)
+    action: Mapped[str] = mapped_column(String(80), nullable=False)
+    target_type: Mapped[Optional[str]] = mapped_column(String(50), nullable=True)
+    target_id: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    project_id: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    request_id: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    ip: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    user_agent: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    outcome: Mapped[str] = mapped_column(String(20), nullable=False, default="success")
+    details: Mapped[Optional[dict]] = mapped_column(JSON, nullable=True)

app/schemas/audit.py
@@ -1,0 +1,34 @@
+"""Audit log responses."""
+
+from datetime import datetime
+from typing import Any, List, Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+class AuditEventResponse(BaseModel):
+    """One audit event, with the acting user's name when the user still exists."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    occurred_at: datetime
+    actor_user_id: Optional[int] = None
+    actor_name: Optional[str] = None
+    actor_type: str
+    action: str
+    target_type: Optional[str] = None
+    target_id: Optional[str] = None
+    project_id: Optional[int] = None
+    request_id: Optional[str] = None
+    ip: Optional[str] = None
+    user_agent: Optional[str] = None
+    outcome: str
+    details: Optional[dict[str, Any]] = None
+
+
+class AuditEventPage(BaseModel):
+    """A page of audit events, newest first, and the total that match the filter."""
+
+    items: List[AuditEventResponse]
+    total: int

ui/src/api/client.ts
@@ -429,6 +429,46 @@ export const groupsApi = {
   },
 }
 
+export interface AuditEvent {
+  id: number
+  occurred_at: string
+  actor_user_id: number | null
+  actor_name: string | null
+  actor_type: string
+  action: string
+  target_type: string | null
+  target_id: string | null
+  project_id: number | null
+  request_id: string | null
+  ip: string | null
+  user_agent: string | null
+  outcome: string
+  details: Record<string, unknown> | null
+}
+
+export interface AuditEventPage {
+  items: AuditEvent[]
+  total: number
+}
+
+export interface AuditFilters {
+  actor_user_id?: number
+  action?: string
+  target_type?: string
+  target_id?: string
+  project_id?: number
+  outcome?: string
+  since?: string
+  until?: string
+  limit?: number
+  offset?: number
+}
+
+export const auditApi = {
+  list: async (filters: AuditFilters = {}) =>
+    (await api.get<AuditEventPage>('/audit', { params: filters })).data,
+}
+
 export interface DashboardStats {
   total_projects: number
   active_projects: number